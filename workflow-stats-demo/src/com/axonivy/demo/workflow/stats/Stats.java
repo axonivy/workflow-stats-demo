31288b887661e660e7393fa106a3889d1706c361
Reto Weiss <reto.weiss@axonivy.com>
2023-02-16 12:08:40 +0100

    Fix demo to run with latest API

--- workflow-stats-demo/src/com/axonivy/demo/workflow/stats/Stats.java
+++ workflow-stats-demo/src/com/axonivy/demo/workflow/stats/Stats.java
@@ -1,15 +1,18 @@
 package com.axonivy.demo.workflow.stats;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import ch.ivyteam.ivy.elasticsearch.client.agg.AggOperator;
+import ch.ivyteam.ivy.elasticsearch.client.agg.AggregationQuery;
+import ch.ivyteam.ivy.elasticsearch.client.agg.AggregationResult;
+import ch.ivyteam.ivy.elasticsearch.client.agg.Bucket;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.stats.WorkflowStats;
-import ch.ivyteam.ivy.workflow.task.TaskBusinessState;
 
 @Path("stats2")
 public class Stats {
@@ -17,17 +20,24 @@ public class Stats {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public Result get() {
-    return new Result(WorkflowStats.current().task().byState().get());
+	var query = AggregationQuery.create().agg(AggOperator.stringBuckets("state")).toAggregationQuery();
+    return new Result(WorkflowStats.current().task().aggregate(query));
   }
 
   public static final class Result {
 
-    public final List<TaskBusinessState> states;
+    public final List<String> states;
     public final List<Long> counts;
 
-    public Result(Map<TaskBusinessState, Long> states) {
-      this.states = states.keySet().stream().toList();
-      this.counts = states.values().stream().toList();
+    public Result(AggregationResult result) {
+      var buckets = result.aggs().get("state");
+      if (buckets == null) {
+    	  this.states = List.of();
+    	  this.counts = List.of();
+    	  return;
+      }
+      this.states = buckets.stream().map(Bucket.class::cast).map(b -> b.key()).toList();
+      this.counts = buckets.stream().map(Bucket.class::cast).map(b -> b.count()).toList();
     }
   }
 }
